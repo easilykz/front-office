0325fdacfa219042b56b0514bc8bbe18ebfc4ba9
temirlanKabylbekov <temirlan.kabylbekov777@gmail.com>
2022-01-27 22:23:08 +0600

    Did подписание

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import json
+
 from flask import Flask, request, render_template
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
@@ -41,21 +44,39 @@ def apply_for_job():
     return render_template('apply_for_job.html')
 
 
+@app.route('/contract-remote', methods=['GET'])
+def remote_apply_for_job():
+    return render_template('apply_for_job.html')
+
+
 @app.route('/contract-doc', methods=['GET'])
 def contract_doc():
     code = request.args.get('code')
     document_url = None
-    next_url = 'https://google.com/'
+    next_url = None
+
+    has_next = request.args.get('n') == '✓'
+
+    if has_next:
+        time.sleep(int(settings.WAITING_FOR_GENERATING_CONTRACT_FILE))
 
     if code:
         try:
             form = HttpClient.get(f'{settings.BACKOFFICE_CONTRACT_URL}={code}').json()
             document_url = form.get('contract_url')
+            next_url = form.get('sign_url')
+        except json.decoder.JSONDecodeError:
+            return render_template('error.html')
         except FrontOfficeHttpError:
-            pass
+            return render_template('error.html')
 
-    has_next = request.args.get('n') == '✓' and document_url is not None
+    has_next = has_next and document_url is not None
     return render_template('contract_doc.html', document_url=document_url, has_next=has_next, next_url=next_url)
+
+
+@app.route('/integrations/did/')
+def sign_finish():
+    return 'hi'
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,40 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+def decrypt(private_key_path, encrypted):
+    with open(private_key_path, 'rb') as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+    return private_key.decrypt(
+        encrypted,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+
+def encrypt(public_key_path, original_message):
+    original_message = str.encode(original_message)
+
+    with open(public_key_path, 'rb') as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+
+    return public_key.encrypt(
+        original_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
